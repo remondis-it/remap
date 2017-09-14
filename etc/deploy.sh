6da7e063bf9cab592ed6e4eb7a0bb914cc41131a
Christopher Schuette <christopher.schuette@remondis.de>
2017-09-14 14:35:05 +0200

    Deploy Cleanup

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -3,12 +3,6 @@ echo "Decrypting Sign Key..."
 
 gpg --passphrase $envOssrhPassword -o ./etc/secring.gpg -d ./etc/sign.enc
 
-echo "Checksumme ./etc/sign.enc" 
-md5sum ./etc/my_key.enc
-echo "Checksumme ./etc/secring.gpg" 
-md5sum ./etc/secring.gpg
-
-
 echo "Starting task 'uploadArchives'..."
 ./gradlew uploadArchives -Prelease
 
