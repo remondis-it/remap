3240201c7ea7b5e98b93dd067591c0d30220dce6
Christopher Schuette <christopher.schuette@remondis.de>
2017-09-14 12:17:47 +0200

    Neuer Versuch

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Decrypting Sign Key..."
 
-openssl aes-256-cbc -pass "pass:$SECRET_ENV" -in ./etc/secring.enc -out ./etc/secring.gpg -d -a 2>&1
+openssl aes-256-cbc -pass "pass:$SECRET_ENV" -in ./etc/my_key.enc -out ./etc/secring.gpg -d -a 2>&1
 
-FILENAME=./etc/secring.gpg
+FILENAME=./etc/my_key.enc
 FILESIZE=$(stat -c%s "$FILENAME")
 echo "Size of $FILENAME = $FILESIZE bytes."
 
