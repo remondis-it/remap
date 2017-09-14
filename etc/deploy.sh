10f203d4cf2f2cb5d300d0a7118c4ba366977497
Christopher Schuette <christopher.schuette@remondis.de>
2017-09-14 10:21:25 +0200

    Signierschlüssel hinzugefügt.
    - Deployscript hinzugefügt.
    - Pfade angepasst.
    - Before-Script hinzugefügt.
    - Release-Switch geschaltet.

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -2,6 +2,10 @@
 echo "Decrypting Sign Key..."
 openssl aes-256-cbc -pass "pass:$SECRET_ENV" -in ./etc/secring.enc -out ./etc/secring -d -a
 
+FILENAME=./etc/secring
+FILESIZE=$(stat -c%s "$FILENAME")
+echo "Size of $FILENAME = $FILESIZE bytes."
+
 echo "Starting task 'uploadArchives'..."
 ./gradlew uploadArchives -Prelease
 
