c3f3f04680e975f9506e8a544662ff5336587727
Christopher Schuette <christopher.schuette@remondis.de>
2017-09-14 10:03:10 +0200

    Signierschlüssel hinzugefügt.
    - Deployscript hinzugefügt.
    - Pfade angepasst.
    - Before-Script hinzugefügt.
    - Release-Switch geschaltet.

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -3,6 +3,6 @@ echo "Decrypting Sign Key..."
 openssl aes-256-cbc -pass "pass:$SECRET_ENV" -in ./etc/secring.enc -out ./etc/secring -d -a
 
 echo "Starting task 'uploadArchives'..."
-./gradlew uploadArchives
+./gradlew uploadArchives -Prelease
 
 rm ./etc/secring
