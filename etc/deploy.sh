4151fcf14ef88dbe622af28885e3560933e591d7
Christopher Schuette <christopher.schuette@remondis.de>
2017-09-14 14:24:21 +0200

    GPG Ansatz

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 echo "Decrypting Sign Key..."
 
-openssl aes-256-cbc -pass "pass:$MY_SECRET_ENV" -in ./etc/my_key.enc -out ./etc/secring.gpg -d -a 2>&1
-
-
-FILENAME=./etc/my_key.enc
-FILESIZE=$(stat -c%s "$FILENAME")
-echo "Size of $FILENAME = $FILESIZE bytes."
-
-FILENAME=./etc/secring.gpg
-FILESIZE=$(stat -c%s "$FILENAME")
-echo "Size of $FILENAME = $FILESIZE bytes."
-
+gpg --passphrase $envOssrhPassword -o original_file.txt -d sign.enc
 
 echo "Checksumme ./etc/my_key.enc" 
 md5sum ./etc/my_key.enc
 echo "Checksumme ./etc/secring.gpg" 
 md5sum ./etc/secring.gpg
-echo "Checksumme of secret" 
-echo -n "$MY_SECRET_ENV" | md5sum
+
 
 echo "Starting task 'uploadArchives'..."
 ./gradlew uploadArchives -Prelease
