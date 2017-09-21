598232a58173f61d61cc44fd33a87105c5fa1685
Christopher Schuette <christopher.schuette@remondis.de>
2017-09-21 08:26:00 +0200

    Flat mapping documented
    - and DemoTest added.

--- src/test/java/com/remondis/remap/FlatCollectionMapping.java
+++ src/test/java/com/remondis/remap/FlatCollectionMapping.java
@@ -24,6 +24,29 @@ import com.remondis.remap.flatCollectionMapping.Source;
 public class FlatCollectionMapping {
 
   @Test
+  public void shouldMapCollectionByFunction() {
+    Mapper<Source, Destination> mapper = Mapping.from(Source.class)
+        .to(Destination.class)
+        .replaceCollection(Source::getIds, Destination::getIds)
+        .with(idBuilder())
+        .mapper();
+
+    Source source = Source.builder()
+        .ids(Arrays.asList(1L, 2L, 3L))
+        .build();
+    Destination map = mapper.map(source);
+    List<Id> expected = Arrays.asList(idBuilder().transform(1L), idBuilder().transform(2L), idBuilder().transform(3L));
+    List<Id> actual = map.getIds();
+    assertEquals(expected, actual);
+
+    // Assert the mapping
+    AssertMapping.of(mapper)
+        .expectReplaceCollection(Source::getIds, Destination::getIds)
+        .andTest(idBuilder())
+        .ensure();
+  }
+
+  @Test
   public void shouldDetectIllegalArguments() {
     assertThatThrownBy(() -> {
       Mapping.from(Source.class)
@@ -46,29 +69,6 @@ public class FlatCollectionMapping {
           .with(null);
     }).isInstanceOf(IllegalArgumentException.class)
         .hasNoCause();
-  }
-
-  @Test
-  public void shouldMapCollectionByFunction() {
-    Mapper<Source, Destination> mapper = Mapping.from(Source.class)
-        .to(Destination.class)
-        .replaceCollection(Source::getIds, Destination::getIds)
-        .with(idBuilder())
-        .mapper();
-
-    Source source = Source.builder()
-        .ids(Arrays.asList(1L, 2L, 3L))
-        .build();
-    Destination map = mapper.map(source);
-    List<Id> expected = Arrays.asList(idBuilder().transform(1L), idBuilder().transform(2L), idBuilder().transform(3L));
-    List<Id> actual = map.getIds();
-    assertEquals(expected, actual);
-
-    // Assert the mapping
-    AssertMapping.of(mapper)
-        .expectReplaceCollection(Source::getIds, Destination::getIds)
-        .andTest(idBuilder())
-        .ensure();
   }
 
   @Test
---
+++ src/test/java/com/remondis/remap/flatCollectionMapping/DemoTest.java
@@ -0,0 +1,32 @@
+package com.remondis.remap.flatCollectionMapping;
+
+import org.junit.Test;
+
+import com.remondis.remap.AssertMapping;
+import com.remondis.remap.Mapper;
+import com.remondis.remap.Mapping;
+import com.remondis.remap.Transform;
+
+public class DemoTest {
+
+  @Test
+  public void replaceCollection() {
+    Mapper<Source, Destination> mapper = Mapping.from(Source.class)
+        .to(Destination.class)
+        .replaceCollection(Source::getIds, Destination::getIds)
+        .with(newId())
+        .mapper();
+
+    AssertMapping.of(mapper)
+        .expectReplaceCollection(Source::getIds, Destination::getIds)
+        .andTest(newId())
+        .ensure();
+  }
+
+  public Transform<Id, Long> newId() {
+    return id -> Id.builder()
+        .id(id)
+        .build();
+  }
+
+}
