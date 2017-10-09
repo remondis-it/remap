515323f008589ff8ac49d1d3b9502562c6a3301b
Christopher Schuette <christopher.schuette@remondis.de>
2017-10-09 14:03:01 +0200

    API fixed andTestButSkipWhenNull
    - API fixed by renaming the method and removing the argument.
    - The project is now using the fixed API
    - Improved the documentation of `ReplaceAssertBuilder`
    - Deprecation removed.
    - This change breaks backwards compatibility
    - Version changed to 2.0.0
    - Merged with develop. Migration to new API.

--- src/main/java/com/remondis/remap/ReplaceAssertBuilder.java
+++ src/main/java/com/remondis/remap/ReplaceAssertBuilder.java
@@ -38,14 +38,14 @@ public class ReplaceAssertBuilder<S, D, RD, RS> {
    * @param transformation The transformation to test.
    * @return Returns the {@link AssertMapping} for further configuration.
    */
-  public AssertMapping<S, D> andTest(Transform<RD, RS> transformation) {
+  public AssertMapping<S, D> andTest(Transform<RS, RD> transformation) {
     denyNull("tranfromation", transformation);
-    ReplaceTransformation<RD, RS> replace = new ReplaceTransformation<RD, RS>(asserts.getMapping(),
-        sourceProperty.property, destProperty.property, transformation, false);
+    ReplaceTransformation<RS, RD> replace = new ReplaceTransformation<>(asserts.getMapping(), sourceProperty.property,
+        destProperty.property, transformation, false);
     asserts.addAssertion(replace);
     return asserts;
   }
- 
+
   /**
    * Expects the mapping to skip the transform function on <code>null</code> input.
    *
