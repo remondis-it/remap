aba3e7be3a4c43facf19e4dbd6da7c05f641119e
schuettec <schuettec@DESKTOP-LPBFIEM.fritz.box>
2018-04-19 19:14:41 +0200

    Bug fix for Boolean property.
    - Regression JUnit test added

---
+++ src/test/java/com/remondis/remap/possibleBug/A.java
@@ -0,0 +1,26 @@
+package com.remondis.remap.possibleBug;
+
+public class A {
+  private Boolean newsletterSubscribed;
+  private String mail;
+
+  public A() {
+    super();
+  }
+
+  public String getMail() {
+    return mail;
+  }
+
+  public void setMail(String mail) {
+    this.mail = mail;
+  }
+
+  public Boolean getNewsletterSubscribed() {
+    return newsletterSubscribed;
+  }
+
+  public void setNewsletterSubscribed(Boolean newsletterSubscribed) {
+    this.newsletterSubscribed = newsletterSubscribed;
+  }
+}
---
+++ src/test/java/com/remondis/remap/possibleBug/B.java
@@ -0,0 +1,18 @@
+package com.remondis.remap.possibleBug;
+
+public class B {
+  private String email;
+
+  public B() {
+    super();
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+}
