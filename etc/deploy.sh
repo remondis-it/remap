607303ad2a6920364716fd3598da331511f141db
Christopher Schuette <christopher.schuette@remondis.de>
2018-10-15 12:36:56 +0200

    Moving to Bintray for publishing

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-echo "Decrypting Sign Key..."
 
-gpg --passphrase $envOssrhPassword -o ./etc/secring.gpg -d ./etc/sign.enc
-
-echo "Starting task 'uploadArchives'..."
-./gradlew uploadArchives -Prelease
-
+echo "Starting deploy task..."
+./gradlew bintrayUpload -Dbintray.user=$BINTRAY_USER -Dbintray.key=$BINTRAY_API_KEY
