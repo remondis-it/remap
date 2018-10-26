887d60fba49c5fdd6a8e7c669f114e8ec395ae2f
Christopher Schuette <christopher.schuette@remondis.de>
2018-10-26 11:34:37 +0200

    Relax the type restriction for replace operation
    - It is now possible to use registered mappers for reassing operations.

---
+++ src/test/java/com/remondis/remap/implicitMappings/differentFieldNames/A.java
@@ -0,0 +1,36 @@
+package com.remondis.remap.implicitMappings.differentFieldNames;
+
+import java.util.List;
+
+public class A {
+
+  private B b;
+  private List<B> bs;
+
+  public A() {
+    super();
+  }
+
+  public A(B b, List<B> bs) {
+    super();
+    this.b = b;
+    this.bs = bs;
+  }
+
+  public B getB() {
+    return b;
+  }
+
+  public void setB(B b) {
+    this.b = b;
+  }
+
+  public List<B> getBs() {
+    return bs;
+  }
+
+  public void setBs(List<B> bs) {
+    this.bs = bs;
+  }
+
+}
