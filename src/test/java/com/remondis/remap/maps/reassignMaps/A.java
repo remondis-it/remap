0e8c9802a98c45d0071ff9d946d30e19d984bdfd
Christopher Schuette <christopher.schuette@remondis.de>
2019-02-06 16:26:58 +0100

    Rassign and implicit mapping of Maps is unsupported
    - Even if a type mapping is specified, the mapping of maps is
    restricted.
    
    com.remondis.remap.maps.reassignMaps.MapperTest demonstrates the
    restriction.

---
+++ src/test/java/com/remondis/remap/maps/reassignMaps/A.java
@@ -0,0 +1,22 @@
+package com.remondis.remap.maps.reassignMaps;
+
+import java.util.Map;
+
+public class A {
+
+  private Map<String, String> map;
+
+  public A() {
+    super();
+
+  }
+
+  public Map<String, String> getMap() {
+    return map;
+  }
+
+  public void setMap(Map<String, String> map) {
+    this.map = map;
+  }
+
+}
