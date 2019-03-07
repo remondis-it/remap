48c5865f172f6d0e69a855c3f7db674d522a5b39
Christopher Schuette <christopher.schuette@remondis.de>
2019-03-07 14:21:53 +0100

    Added the complete omit-Feature
    - Added to Mapping API omitOthers, omitOtherDestinationProperties,
    omitOtherSourceProperties
    - Added to AssertMapping API expectOthersToBeOmitted,
    expectOtherDestinationFieldsToBeOmitted,
    expectOtherSourceFieldsToBeOmitted
    
    And tests in com.remondis.remap.omitOthers.MapperTest

---
+++ src/test/java/com/remondis/remap/assertion/BResource.java
@@ -0,0 +1,54 @@
+package com.remondis.remap.assertion;
+
+public class BResource {
+
+  private String string;
+
+  public BResource() {
+    super();
+  }
+
+  public BResource(String string) {
+    super();
+    this.string = string;
+  }
+
+  public String getString() {
+    return string;
+  }
+
+  public void setString(String string) {
+    this.string = string;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((string == null) ? 0 : string.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    BResource other = (BResource) obj;
+    if (string == null) {
+      if (other.string != null)
+        return false;
+    } else if (!string.equals(other.string))
+      return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "BResource [string=" + string + "]";
+  }
+
+}
