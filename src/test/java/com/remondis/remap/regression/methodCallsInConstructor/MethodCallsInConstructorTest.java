7779c5230db2dfaf8b41de2bc2edeb9093be8d3b
Christopher Schuette <christopher.schuette@remondis.de>
2019-05-15 12:30:38 +0200

    Test that shows call in constructor bug.

---
+++ src/test/java/com/remondis/remap/regression/methodCallsInConstructor/MethodCallsInConstructorTest.java
@@ -0,0 +1,24 @@
+package com.remondis.remap.regression.methodCallsInConstructor;
+
+import java.util.function.Function;
+
+import org.junit.Test;
+
+import com.remondis.remap.Mapper;
+import com.remondis.remap.Mapping;
+
+public class MethodCallsInConstructorTest {
+
+  @Test
+  public void shouldNotComplainAboutInternalMethodsInConstructor() {
+    Mapper<A, A> mapper = Mapping.from(A.class)
+        .to(A.class)
+        .replace(A::getaString, A::getaString)
+        .with(Function.identity())
+        .mapper();
+
+    A mapped = mapper.map(new A("AString"));
+
+  }
+
+}
