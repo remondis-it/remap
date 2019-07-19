40d53d629c815724966fd5742adfdd6e608f5f1d
Christopher Schuette <christopher.schuette@remondis.de>
2019-07-19 09:27:40 +0200

    Bug: Using useMapper() twice produces a wrong exception message.
    - Fixed
    - Regression test added.

---
+++ src/test/java/com/remondis/remap/regression/useMapperTwice/BMapped.java
@@ -0,0 +1,27 @@
+package com.remondis.remap.regression.useMapperTwice;
+
+/**
+ *
+ */
+public class BMapped {
+
+  private int stringLength;
+
+  public BMapped(int stringLength) {
+    super();
+    this.stringLength = stringLength;
+  }
+
+  public BMapped() {
+    super();
+  }
+
+  public int getStringLength() {
+    return stringLength;
+  }
+
+  public void setStringLength(int stringLength) {
+    this.stringLength = stringLength;
+  }
+
+}
