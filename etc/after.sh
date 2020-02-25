49a60f93eef20a98b77523aa40e464e9aae3f62c
Christopher Schuette <christopher.schuette@remondis.de>
2020-02-25 09:29:06 +0100

    Auto-sync Maven Central in Bintray after deploy

---
+++ etc/after.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Syncing Maven Central repository"
+
+version=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep "^[[:digit:]]*[[:digit:]].[[:digit:]]*[[:digit:]].[[:digit:]]*[[:digit:]]"`
+
+echo "Syncing project version ${version}"
+
+curl --verbose  --user 'schuettec:${BINTRAY_API_KEY}' --request "POST" "https://api.bintray.com/maven_central_sync/schuettec/maven/com.remondis:remap/versions/${version}"
+
+echo "Sync finished."
