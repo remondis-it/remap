ae706570f192c3a71c5ced241c6cb99a4f8cbafe
Karl Ender <karlender24@gmail.com>
2022-09-13 11:13:45 +0200

    Migrate from cglib to ByteBuddy to support JDK versions 17+

--- src/main/java/com/remondis/remap/InvocationSensor.java
+++ src/main/java/com/remondis/remap/InvocationSensor.java
@@ -1,18 +1,21 @@
 package com.remondis.remap;
 
-import static com.remondis.remap.ReflectionUtil.defaultValue;
-import static com.remondis.remap.ReflectionUtil.hasReturnType;
-import static com.remondis.remap.ReflectionUtil.invokeMethodProxySafe;
-import static com.remondis.remap.ReflectionUtil.isGetter;
-import static com.remondis.remap.ReflectionUtil.toPropertyName;
+import static com.remondis.remap.ReflectionUtil.*;
+import static java.lang.ClassLoader.getSystemClassLoader;
+import static net.bytebuddy.matcher.ElementMatchers.isDeclaredBy;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
-import net.sf.cglib.proxy.Enhancer;
-import net.sf.cglib.proxy.InvocationHandler;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.implementation.bind.annotation.AllArguments;
+import net.bytebuddy.implementation.bind.annotation.Origin;
+import net.bytebuddy.implementation.bind.annotation.RuntimeType;
 
 /**
  * The {@link InvocationSensor} tracks get-method invocations on a proxy class and makes the invocation information
@@ -20,36 +23,58 @@ import net.sf.cglib.proxy.InvocationHandler;
  *
  * @author schuettec
  */
-class InvocationSensor<T> {
+public class InvocationSensor<T> {
 
   private T proxyObject;
 
   private List<String> propertyNames = new LinkedList<>();
 
-  InvocationSensor(Class<T> superType) {
-    Enhancer enhancer = new Enhancer();
-    enhancer.setSuperclass(superType);
-    enhancer.setCallback(new InvocationHandler() {
-      @Override
-      public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        if (isGetter(method)) {
-          denyNoReturnType(method);
-          // schuettec - Get property name from method and mark this property as called.
-          String propertyName = toPropertyName(method);
-          propertyNames.add(propertyName);
-          // schuettec - Then return an appropriate default value.
-          return nullOrDefaultValue(method.getReturnType());
-        } else if (isObjectMethod(method)) {
-          // schuettec - 08.02.2017 : Methods like toString, equals or hashcode are redirected to this invocation
-          // handler.
-          return invokeMethodProxySafe(method, this, args);
-        } else {
-          return nullOrDefaultValue(method.getReturnType());
-        }
-      }
+  /**
+   * Creates a proxy for the given class type.
+   *
+   * @param superType the class type for which the proxy should be created
+   */
+  public InvocationSensor(Class<T> superType) {
+    T po = null;
+    try {
+      po = (T) new ByteBuddy().subclass(superType)
+          .method(isDeclaredBy(superType))
+          .intercept(MethodDelegation.to(this))
+          .make()
+          .load(getSystemClassLoader(), ClassLoadingStrategy.Default.INJECTION)
+          .getLoaded()
+          .getDeclaredConstructor()
+          .newInstance();
+    } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException ex) {
+      throw new MappingException(
+          String.format("Error while creating proxy for class '%s'", superType.getCanonicalName()), ex);
+    }
+    proxyObject = po;
+  }
 
-    });
-    proxyObject = superType.cast(enhancer.create());
+  /**
+   * This method will be called each time when the object proxy calls any of its methods.
+   * 
+   * @param method the intercepted method
+   * @param args the given arguments ti the intercepted method
+   * @throws Exception throws
+   */
+  @RuntimeType
+  public Object intercept(@Origin Method method, @AllArguments Object[] args) throws Exception {
+    if (isGetter(method)) {
+      denyNoReturnType(method);
+      // schuettec - Get property name from method and mark this property as called.
+      String propertyName = toPropertyName(method);
+      propertyNames.add(propertyName);
+      // schuettec - Then return an appropriate default value.
+      return nullOrDefaultValue(method.getReturnType());
+    } else if (isObjectMethod(method)) {
+      // schuettec - 08.02.2017 : Methods like toString, equals or hashcode are redirected to this invocation
+      // handler.
+      return invokeMethodProxySafe(method, this, args);
+    } else {
+      return nullOrDefaultValue(method.getReturnType());
+    }
   }
 
   /**
