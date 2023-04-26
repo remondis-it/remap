b908f787719cd3383d7ea38a16f4a1a9a42c0173
Christopher Schütte <christopher.schuette@remondis.de>
2023-04-26 08:59:58 +0200

    Property walker and MapOver Util tested

--- src/test/java/com/remondis/remap/BiRecursivePropertyWalkerTest.java
+++ src/test/java/com/remondis/remap/BiRecursivePropertyWalkerTest.java
@@ -1,5 +1,7 @@
 package com.remondis.remap;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 
@@ -12,6 +14,7 @@ import com.remondis.remap.utils.propertywalker.PropertyAccess;
 import com.remondis.remap.utils.propertywalker.VisitorFunction;
 
 public class BiRecursivePropertyWalkerTest {
+
   @Test
   public void shouldTraverseProperties() {
     Function<A, String> getter = A::getString;
@@ -22,10 +25,10 @@ public class BiRecursivePropertyWalkerTest {
 
           @Override
           public void consume(PropertyAccess<A, String> access) {
-            System.out.println("Source a1.getString()" + access.sourceProperty()
-                .get() + " Target: "
-                + access.targetProperty()
-                    .get());
+            access.sourceProperty()
+                .set("changed1");
+            access.targetProperty()
+                .set("changed2");
           }
         })
         .goInto(A::getB, A::setB, B.class)
@@ -33,10 +36,10 @@ public class BiRecursivePropertyWalkerTest {
 
           @Override
           public void consume(PropertyAccess<B, String> access) {
-            System.out.println("Source a1.getString()" + access.sourceProperty()
-                .get() + " Target: "
-                + access.targetProperty()
-                    .get());
+            access.sourceProperty()
+                .set("changed3");
+            access.targetProperty()
+                .set("changed4");
           }
         })
         .build();
@@ -44,5 +47,12 @@ public class BiRecursivePropertyWalkerTest {
     A a1 = new A("moreInA1", "stringA1", 1, 2, 1L, new B("stringB1", 1, 1));
     A a2 = new A("moreInA2", "stringA2", 1, 2, 1L, new B("stringB2", 1, 1));
     walker.execute(a1, a2);
+
+    assertEquals("changed1", a1.getString());
+    assertEquals("changed2", a2.getString());
+    assertEquals("changed3", a1.getB()
+        .getString());
+    assertEquals("changed4", a2.getB()
+        .getString());
   }
 }
