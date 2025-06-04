9ddf581d47cb0bb1528199f6bf32ef69678be2ae
Christopher Schütte <christopher.schuette@remondis.de>
2025-06-04 11:48:55 +0200

    Tests extended

--- src/test/java/com/remondis/remap/records/MappingRecordsTest.java
+++ src/test/java/com/remondis/remap/records/MappingRecordsTest.java
@@ -2,6 +2,9 @@ package com.remondis.remap.records;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 
 import com.remondis.remap.Mapper;
@@ -25,11 +28,9 @@ public class MappingRecordsTest {
 
   @Test
   public void test_mapping_into_record() {
-
     Mapper<TestClass, TestRecord> mapper = Mapping.from(TestClass.class)
         .to(TestRecord.class)
         .mapper();
-
     TestClass input = new TestClass();
     input.setNummer("123");
     TestRecord actual = mapper.map(input);
@@ -37,7 +38,6 @@ public class MappingRecordsTest {
     assertThat(actual).isNotNull()
         .extracting(TestRecord::nummer)
         .isEqualTo("123");
-
   }
 
   @Test
@@ -63,6 +63,33 @@ public class MappingRecordsTest {
         .extracting(TestClassDestinationParent::getChild)
         .extracting(TestRecord::nummer)
         .isEqualTo("123");
+
+  }
+
+  @Test
+  public void test_mapping_into_nested_record_List() {
+
+    Mapper<TestClassChild, TestRecord> mapperNested = Mapping.from(TestClassChild.class)
+        .to(TestRecord.class)
+        .mapper();
+    Mapper<TestClassListSourceParent, TestClassListDestinationParent> mapperParent = Mapping
+        .from(TestClassListSourceParent.class)
+        .to(TestClassListDestinationParent.class)
+        .useMapper(mapperNested)
+        .mapper();
+
+    String expected1 = "1";
+    String expected2 = "2";
+    String expected3 = "3";
+
+    TestClassListSourceParent input = new TestClassListSourceParent();
+    input
+        .setChild(List.of(new TestClassChild(expected1), new TestClassChild(expected2), new TestClassChild(expected3)));
+    TestClassListDestinationParent actual = mapperParent.map(input);
+
+    assertThat(actual).isNotNull();
+    assertThat(actual.getChild()).extracting(TestRecord::nummer)
+        .contains("1", "2", "3");
 
   }
 
@@ -108,6 +135,15 @@ public class MappingRecordsTest {
   public static class TestClassChild {
     private String nummer;
 
+    public TestClassChild() {
+      super();
+    }
+
+    public TestClassChild(String nummer) {
+      super();
+      this.nummer = nummer;
+    }
+
     public String getNummer() {
       return nummer;
     }
@@ -115,5 +151,31 @@ public class MappingRecordsTest {
     public void setNummer(String nummer) {
       this.nummer = nummer;
     }
+  }
+
+  public static class TestClassListDestinationParent {
+    private Set<TestRecord> child;
+
+    public Set<TestRecord> getChild() {
+      return child;
+    }
+
+    public void setChild(Set<TestRecord> child) {
+      this.child = child;
+    }
+
+  }
+
+  public static class TestClassListSourceParent {
+    private List<TestClassChild> child;
+
+    public List<TestClassChild> getChild() {
+      return child;
+    }
+
+    public void setChild(List<TestClassChild> child) {
+      this.child = child;
+    }
+
   }
 }
